ca23fc0c1b84c0eabcfde67609cbfb22e796fb13
Arrinal S <arrinalsf@gmail.com>
2022-10-12 11:47:50 +0700

    change text test

--- K-MERCH/View Model/ViewModel.swift
+++ K-MERCH/View Model/ViewModel.swift
@@ -19,7 +19,7 @@ class ViewModel: ObservableObject {
     @Published var provinceList = [Province]()
     @Published var cityList = [City]()
     @Published var deliveryMenu = [Delivery]()
-    @Published var text = ""
+    @Published var text = "bagong"
     @Published var finalBuy = FinalBuy()
     @Published var orderPlaced = OrderPlaced()
     @Published var orderList = [OrderPlaced]()
